8cc60435fe23e42e4cf93b9ac2627eaaf17416ab
ddelacruz89 <47066559+ddelacruz89@users.noreply.github.com>
2025-11-05 22:27:14 -0400

    commit

---
+++ eFacturador/src/main/java/com/braintech/eFacturador/jpa/SuperClass/BaseEntityPk.java
@@ -0,0 +1,22 @@
+package com.braintech.eFacturador.jpa.SuperClass;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntityPk {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="empresa_id", nullable = false)
+    private Integer empresaId;
+
+    @Column(name="secuencia",nullable = false)
+    private Integer secuencia;
+
+}
