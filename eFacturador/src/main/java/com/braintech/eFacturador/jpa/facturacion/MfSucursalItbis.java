0c8e81950e36e89cedaabdd6c174d5878c17021c
aner.santana@nordstrom.com <aner.santana@nordstrom.com>
2025-11-12 13:42:24 -0400

    Refactor MgItbis and related services; update models and API endpoints for improved structure and functionality

--- eFacturador/src/main/java/com/braintech/eFacturador/jpa/facturacion/MfSucursalItbis.java
+++ eFacturador/src/main/java/com/braintech/eFacturador/jpa/facturacion/MfSucursalItbis.java
@@ -1,29 +1,32 @@
 package com.braintech.eFacturador.jpa.facturacion;
 
 import com.braintech.eFacturador.jpa.SuperClass.BaseEntity;
-import jakarta.persistence.Column;
+import com.braintech.eFacturador.jpa.general.MgItbis;
+import com.braintech.eFacturador.jpa.producto.MgCategoria;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import lombok.*;
 
 @Entity
-@Table(name = "mg_itbis", schema = "facturacion")
+@Table(name = "mf_itbis", schema = "facturacion")
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class MgItbis extends BaseEntity implements Serializable {
+public class MfItbis extends BaseEntity implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
-  @Id
-  @Column(name = "id")
-  private Integer id;
-
   private String nombre;
+
   private BigDecimal itbis;
+
+  @JoinColumn(name = "mg_itbis_id", referencedColumnName = "id")
+  @ManyToOne(optional = false)
+  private MgItbis mgItbisId;
 }
---
+++ eFacturador/src/main/java/com/braintech/eFacturador/jpa/general/MgItbis.java
@@ -0,0 +1,4 @@
+package com.braintech.eFacturador.jpa.general;
+
+public class MgItbis {
+}
